15e5f3f3346bf3ba3e20e1f2d348e55299b85bbf
Rafael Gomez <rgomez2014@gmail.com>
2023-10-22 16:30:08 -0500

    Reaction schema made

---
+++ models/reactions.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose')
+
+const reactionsSchema = new Schema(
+  {
+    reactionId:{
+      type: Schema.Types.ObjectId,
+      default: new Types.ObjectId()
+    },
+    reactionBody:{
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username:{
+      type: String,
+      required: true
+    },
+    createdAt:{
+      type: Date,
+      default: Date.now,
+      get: createdAtValue =>  createdAtValue.toString(),
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+module.exports = reactionsSchema
