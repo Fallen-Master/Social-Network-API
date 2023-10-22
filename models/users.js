9bbef1a0e65b422d66d4e213203d89e4062459f3
Rafael Gomez <rgomez2014@gmail.com>
2023-10-22 16:30:48 -0500

    spacing and export

--- models/users.js
+++ models/users.js
@@ -1,54 +1,40 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema(
-    {
-        username:{
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        email:{
-            type: String,
-            required: true,
-            unique:true,
-            match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-        },
-        thoughts:[{
-            type: Schema.Types.ObjectId,
-            ref: 'Thoughts'
-        }],
-        friends:[{
-            type: Schema.Types.ObjectId,
-            ref: 'user'
-        }]
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
     },
-    {
-        toJSON: {
-            virtuals:true
-        },
-        id:false
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thoughts'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
 );
 
 userSchema.virtual('friendCount').get(function () {
-    return this.friends.length
+  return this.friends.length
 });
-// username
 
-// String
-// Unique
-// Required
-// Trimmed
-// email
+const User = model('User', userSchema)
 
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
-
-// Array of _id values referencing the Thought model
-// friends
-
-// Array of _id values referencing the User model (self-reference)
+module.exports = User
