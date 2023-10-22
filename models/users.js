e1f91d6cc31a60762568e44e07a9d7b66763277c
Rafael Gomez <rgomez2014@gmail.com>
2023-10-21 19:19:54 -0500

    started users

---
+++ models/users.js
@@ -0,0 +1,38 @@
+User:
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username:{
+
+        },
+        email:{
+
+        },
+        thoughts:{
+            //foriegn id
+        },
+        friends:{
+            //foriegn id
+        }
+
+    }
+)
+// username
+
+// String
+// Unique
+// Required
+// Trimmed
+// email
+
+// String
+// Required
+// Unique
+// Must match a valid email address (look into Mongoose's matching validation)
+// thoughts
+
+// Array of _id values referencing the Thought model
+// friends
+
+// Array of _id values referencing the User model (self-reference)
